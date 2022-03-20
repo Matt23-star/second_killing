9982fce4a37ac7c289b82bad8ddb1a08fc7f3d97
“gao” <976292406@qq.com>
2022-03-20 20:46:43 +0800

    rocketMq added

---
+++ src/main/java/com/example/secondkill/controller/MQController.java
@@ -0,0 +1,50 @@
+package com.example.secondkill.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.example.secondkill.entity.pojo.UserKillState;
+import com.example.secondkill.rocketmq.MQService;
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
+import java.util.UUID;
+
+/**
+ * @author zjl(1552217634 @ qq.com)
+ * @date 2022/3/20 19:46
+ */
+@RestController
+public class MQController {
+
+    @Autowired
+    private MQService mqService;
+
+
+    @RequestMapping("/mq/testSend")
+    public String mqTestSend(){
+        UserKillState userKillState = new UserKillState();
+        userKillState.setId(UUID.randomUUID().toString().replaceAll("-", "").toUpperCase().substring(0, 32));
+        userKillState.setUserId("2618271127218");
+        userKillState.setKillActivityId("57845445");
+        userKillState.setState("购买成功");
+        Object result = null;
+        try {
+            result = mqService.send(JSONObject.toJSONString(userKillState));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (RemotingException e) {
+            e.printStackTrace();
+        } catch (MQBrokerException e) {
+            e.printStackTrace();
+        } catch (MQClientException e) {
+            e.printStackTrace();
+        }
+        return JSONObject.toJSONString(result);
+    }
+}
---
+++ src/main/java/com/example/secondkill/rocketmq/MQService.java
@@ -0,0 +1,38 @@
+package com.example.secondkill.rocketmq;
+
+import com.example.secondkill.utils.ResultUtils;
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * @author zjl(1552217634 @ qq.com)
+ * @date 2022/3/20 19:37
+ */
+@Service
+public class MQService {
+
+    @Autowired
+    private MsgProducer producer;
+
+    public Object send(String msg) throws UnsupportedEncodingException, InterruptedException, RemotingException, MQBrokerException, MQClientException {
+
+        // 创建一个消息实例，包含 topic、tag 和 消息体
+        Message message=new Message("testTopic","",msg.getBytes(RemotingHelper.DEFAULT_CHARSET));
+
+        //调用方法==>返回消息体
+        SendResult result =producer.getProducer().send(message);
+
+        System.out.println("发送响应消息ID:" + result.getMsgId() + "，发送状态:" + result.getSendStatus());
+
+        return ResultUtils.success();
+    }
+
+}
