c92874f612371a74537dba141fc6e911e02c90d8
“gao” <976292406@qq.com>
2022-03-23 14:59:05 +0800

    新建将秒杀状态从redis中存入数据库中的线程

--- src/main/java/com/example/secondkill/service/impl/User_kill_stateServiceImpl.java
+++ src/main/java/com/example/secondkill/service/impl/User_kill_stateServiceImpl.java
@@ -4,7 +4,11 @@ import com.example.secondkill.entity.pojo.UserKillState;
 import com.example.secondkill.mapper.User_kill_stateMapper;
 import com.example.secondkill.service.IUser_kill_stateService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 /**
  * <p>
@@ -16,5 +20,37 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class User_kill_stateServiceImpl extends ServiceImpl<User_kill_stateMapper, UserKillState> implements IUser_kill_stateService {
+
+    @Autowired
+    private RedisTemplate<Object, Object> redisTemplate;
+
+    @Autowired
+    private RedisTemplate<String, String> stringStringRedisTemplate;
+
+    @Autowired
+    private User_kill_stateMapper userKillStateMapper;
+
+    @PostConstruct
+    public void init() {
+        // 此线程将秒杀状态从redis中存入数据库中
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                String killId = stringStringRedisTemplate.opsForSet().randomMember("secondKills");
+                if (killId != null) {
+                    for (int i = 0; i < 120; i++) {
+                        UserKillState userKillState =
+                                (UserKillState) redisTemplate.opsForList().rightPop(killId + ".userStateList");
+                        if (userKillState == null) break;
+                        else userKillStateMapper.insert(userKillState);
+                    }
+                }
+            }
+        }).start();
+    }
 
 }
