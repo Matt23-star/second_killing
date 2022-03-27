c9836914d7018b171a340c9737ad21d14fcbd539
Jialong <1552217634@qq.com>
2022-03-27 21:25:16 +0800

    用户访问后端接口前进行token校验

---
+++ src/main/java/com/example/secondkill/config/interceptor/UserAuthInterceptor.java
@@ -0,0 +1,49 @@
+package com.example.secondkill.config.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author zjl(1552217634 @ qq.com)
+ * @date 2022/3/27 20:28
+ */
+@Component
+public class UserAuthInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //统一拦截
+        Cookie[] cookies = request.getCookies();
+        Cookie token=null;
+        for (Cookie cookie : cookies) {
+            if ("userToken".equals(cookie.getName())){
+                token=cookie;
+                break;
+            }
+        }
+        if(token==null)return false;
+        String uid = (String)redisTemplate.opsForValue().get(token.getValue() + "token");
+        if(uid==null)return false;
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
